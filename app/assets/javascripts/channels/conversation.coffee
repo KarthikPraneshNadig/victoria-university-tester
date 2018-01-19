b4bf0b80d24bd4c630e0ba6d34cf0dfb3104c7b7
Anthematics <jasonharder2@gmail.com>
2018-01-19 17:45:17 -0500

    failing consolelog test

--- app/assets/javascripts/channels/conversation.coffee
+++ app/assets/javascripts/channels/conversation.coffee
@@ -4,18 +4,21 @@ App.conversation = App.cable.subscriptions.create("ConversationChannel", {
   received: function(data) {
     console.log(data['message']);
   },
+  
   speak: function(message) {
     return this.perform('speak', {
       message: message
     });
   }
 });
+
 $(document).on('submit', '.new_message', function(e) {
   e.preventDefault();
   let values = $(this).serializeArray();
   App.conversation.speak(values);
   $(this).trigger('reset');
 });
+
 
 #speak runs the speak method on the back end which will send a object that includes a passed parameter (for example the message)
 # the received method will log the result on the back end 
--- app/channels/conversation_channel.rb
+++ app/channels/conversation_channel.rb
@@ -1,24 +1,21 @@
 class ConversationChannel < ApplicationCable::Channel
+#  this subscribed method allows for a unique channel per id
   def subscribed
-    stream from "conversations-#{current_user.id}"
+    stream_from "conversations-#{current_user.id}"
   end
-#The subscribe method creates a unique channel for each user because only the people using the channel should be notified 
-#about events that are performed in it , also the sender and receiver get different information so we always need to notify 2 channels after we code.
+
+#unsubscribed spreaks for itself 
   def unsubscribed
     stop_all_streams
   end
-#removes all connections 
 
-  def speak(data)
+#speak creates a hash based on passed data and sends it to the front end with broadcase
+# data from here can be seen on the front end
+def speak(data)
     message_params = data['message'].each_with_object({}) do |el, hash|
-      hash[el.values.first]= el.values.last
-    end 
-  
-    ActionCable.server.broadcast(
-      "conversations-#{current_user.id}",
-      message: message_params
-    )
-    #speak builds a hash that is based on passed data that sends information to the front end using action cable specified to the channel this is visible on the front ends received method
+      hash[el.values.first] = el.values.last
+    end
+
+    Message.create(message_params)
   end
 end
-
